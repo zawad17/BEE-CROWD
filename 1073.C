292712e3475e44cb7e364e6c7ebf43666bef90f1
Zawad Rahman <138771190+zawad17@users.noreply.github.com>
2024-03-23 23:38:38 +0600

    1073.C

---
+++ 1073.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+
+    int n;
+    scanf("%d",&n);
+    for(int i=1; i<=n; i=i+1)
+    {
+        if(i%2==0)
+        {
+            printf("%d^2 = %d\n",i,i*i);
+        }
+    }
+
+    return 0;
+}
