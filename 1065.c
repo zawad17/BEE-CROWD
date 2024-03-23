f220b2bc11639fb491a855d21daedc5b46ac67dc
Zawad Rahman <138771190+zawad17@users.noreply.github.com>
2024-03-24 00:46:13 +0600

    1065.c

---
+++ 1065.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+
+    int n,count=0;
+    for(int i=1; i<=5; i++)
+    {
+        scanf("%d",&n);
+        if(n%2==0)
+        {
+            count++;
+        }
+    }
+printf("%d valores pares\n",count);
+
+    return 0;
+}
